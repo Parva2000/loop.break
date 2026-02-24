9f2d1baa7070dc10ffd898e661c067a936108f47
Parva2000 <par.pirooz@gmail.com>
2026-02-24 11:19:10 +0100

    Create Untitled-1.py

---
+++ Untitled-1.py
@@ -0,0 +1,8 @@
+items_on_sale = ["blue shirt","striped socks","knit dress","red headband","dinosaur onesie"]
+
+print("checking in the sale list!")                 
+for item in items_on_sale :
+    print (item)
+    if item == "knit dress":
+        break
+print ("end of search! ")
